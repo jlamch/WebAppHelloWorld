e54fc2c9701d7d6dd62f025accae32c6fc49a624
Joanna Lamch <joanna.lamch@billennium.pl>
2017-03-17 21:15:21 +0100

    initial commit

---
+++ WebAppHelloWorld/WebAppHelloWorld/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppHelloWorld
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
